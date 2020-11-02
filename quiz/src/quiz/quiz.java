e05a0f55891bbd026a38f0c315e793c1ca6398ea
ASUS <ASUS@LAPTOP-K09EP18M>
2020-11-02 13:05:26 +0800

    quiz

--- quiz/src/quiz/quiz.java
+++ quiz/src/quiz/quiz.java
@@ -15,20 +15,20 @@ public class quiz {
 	public static void female() {
 		Scanner in = new Scanner (System.in);
 		
-		System.out.print("Input female high in feet: ");
+		System.out.print("Input female height in feet: ");
 		int feet = in.nextInt();
-		System.out.print("Input female high in inch: ");
+		System.out.print("Input female height in inch: ");
 		int inch = in.nextInt();
-		System.out.println("The ideal weight for a female is " + (40 + (inch*5)) + "kg");
+		System.out.println("The ideal weight for a female is " + (40 + ((feet-5)*12*6) + (inch*5)) + "kg");
 		
 	}
 	
 	public static void male() {
 		Scanner in = new Scanner (System.in);
 		
-		System.out.print("Input male high in feet: ");
+		System.out.print("Input male height in feet: ");
 		int feet = in.nextInt();
-		System.out.print("Input male high in inch: ");
+		System.out.print("Input male height in inch: ");
 		int inch = in.nextInt();
 		System.out.println("The ideal weight for a male is " + (50 + ((feet-5)*12*6) + (inch*6)) + "kg");
 	
